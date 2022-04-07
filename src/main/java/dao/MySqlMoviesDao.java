d5559ac2e796f89ab7379f2410a029f18c446b09
Scott Carpenter <carpenter.scott22@gmail.com>
2022-04-07 17:04:00 -0500

    set up Config object constructor in MySqlMovieDao.java

--- src/main/java/dao/MySqlMoviesDao.java
+++ src/main/java/dao/MySqlMoviesDao.java
@@ -1,11 +1,32 @@
 package dao;
 
+import com.mysql.cj.jdbc.Driver;
+import config.Config;
 import data.Movie;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.List;
 
 public class MySqlMoviesDao implements MoviesDao{
+    private Connection connection = null;
+    Config config = new Config();
+    public MySqlMoviesDao(Config config) {
+        try {
+            DriverManager.registerDriver(new Driver());
+
+            this.connection = DriverManager.getConnection(
+                    config.getUrl(),
+                    config.getUser(),
+                    config.getPassword()
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException("Error connecting to the database!", e);
+        }
+    }
+
+
     @Override
     public List<Movie> all() throws SQLException {
         return null;
